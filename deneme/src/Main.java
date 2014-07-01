2eba557d720700f7a8bdf9ca04d2f4f8122d3aed
testuser <testuser@debian>
2014-07-01 11:12:24 +0000

    a

--- deneme/src/Main.java
+++ deneme/src/Main.java
@@ -1,19 +1,17 @@
-
 public class Main {
-	public static void main(String[] args) {
+
+	
+		public static void main(String[] args) {
 		System.out.println("aaaa");
-		
-		System.out.println("ffff");
-		
+System.out.println("ffff");
+
 		int x = 0;
-		for (int i = 0 ; i < 100 ; i++) {
+		for (int i = 0; i < 100; i++) {
 			x += i;
 		}
-		
-		System.out.println(""+x);
-		
+		System.out.println("" + x);
+
 		System.out.println(Test.topla(3, 5));
-		
-		
+
 	}
 }
