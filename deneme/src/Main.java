1c70f0ce765220cfc20f9ef51e524cdb5b6a5f66
testuser <testuser@debian>
2014-07-01 10:57:04 +0000

    yeni kod eklendi

--- deneme/src/Main.java
+++ deneme/src/Main.java
@@ -2,5 +2,14 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("aaaa");
+		
+		System.out.println("ffff");
+		
+		int x = 0;
+		for (int i = 0 ; i < 100 ; i++) {
+			x += i;
+		}
+		System.out.println(""+x);
+		
 	}
 }
